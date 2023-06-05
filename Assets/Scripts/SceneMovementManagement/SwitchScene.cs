d655129738855306c8ec3aa387bd49c97ca854ab
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-06-05 14:15:54 +0700

    Add Pause scene and Pause button

--- Assets/Scripts/SceneMovementManagement/SwitchScene.cs
+++ Assets/Scripts/SceneMovementManagement/SwitchScene.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    private string sceneName = "GameScene";
-    public void Switch()
+    private string maingame = "GameScene";
+    private string pausescene = "PauseScene";
+    private string mainmenu = "MainMenu";
+
+    public void SwitchtoMainGame()
     {
-      SceneManager.LoadScene(sceneName);
+      SceneManager.LoadScene(maingame);
+    }
+    public void SwitchtoPause()
+    {
+        SceneManager.LoadScene(pausescene);
+    }
+    public void SwitchtoMainMenu()
+    {
+        SceneManager.LoadScene(mainmenu);
     }
 }
