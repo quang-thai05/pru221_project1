0512f1bc461775d8295d40dbb161902396de92b4
hungbao1 <hunghxhe153113@fpt.edu.vn>
2023-06-06 15:59:58 +0700

    Update sound click for pause panel

---
+++ Assets/Scripts/SaveLoad/PauseAndGameOverSound.cs
@@ -0,0 +1,20 @@
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseAndGameOverSound : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private Image img;
+    [SerializeField] AudioClip click;
+    [SerializeField] AudioSource source;
+    // Start is called before the first frame update
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        source.PlayOneShot(click);
+    }
+
+
+}
